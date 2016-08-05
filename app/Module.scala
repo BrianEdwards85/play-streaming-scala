a0e6483619100c7ce6e36b0cad4b218e39393478
Brian Edwards <brian.edwards@yodle.com>
2016-08-05 14:25:11 -0500

    more fixes

--- app/Module.scala
+++ app/Module.scala
@@ -1,15 +1,20 @@
+import javax.net.ssl.{SSLContext, SSLSocketFactory}
+
 import com.google.inject.AbstractModule
-import org.eclipse.paho.client.mqttv3.{IMqttClient, MqttClient}
+import org.eclipse.paho.client.mqttv3.{IMqttClient, MqttClient, MqttConnectOptions}
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
 class Module extends AbstractModule{
 
-  val brokerUrl = "tcp://localhost:1883"
+  val brokerUrl = "tcp://edwardstx.us:1883"
   val persistence = new MemoryPersistence
 
   override def configure(): Unit = {
     val client = new MqttClient(brokerUrl, MqttClient.generateClientId, persistence)
-    client.connect
+    val options: MqttConnectOptions = new  MqttConnectOptions();
+    options.setUserName("reverb")
+    options.setPassword("yodle123".toCharArray)
+    client.connect(options)
 
     bind(classOf[IMqttClient])
       .toInstance(client)
